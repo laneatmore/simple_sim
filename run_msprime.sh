d28c3e4b5a014982d8a2a7e3607fa7f29d244896
laneatmore <52425233+laneatmore@users.noreply.github.com>
2021-11-08 08:14:32 -0600

    Create run_msprime.sh

---
+++ run_msprime.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH --account=nn9244k
+#SBATCH --time=01:00:00
+#SBATCH --job-name=msprime
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+
+#set -o errexit
+
+
+module --quiet purge
+module load Anaconda3/2019.03
+module load PLINK/1.9b_6.13-x86_64
+module load Perl/5.32.0-GCCcore-10.2.0
+module load ADMIXTURE/1.3.0
+export PS1=\$
+
+source ${EBROOTANACONDA3}/etc/profile.d/conda.sh
+
+conda deactivate &>/dev/null
+conda activate /cluster/projects/nn9244k/for_lane/msprime-env
+
+
+#for chr {1..22}; do for size in $(less sample_size.list); \
+#do for dem in $(less dems.list); do \
+#sbatch run_msprime.sh $chr $dem $size $size $size; done; done; done
+
+
+#chrom=$1
+#dem_option=$2
+#pop1_sample_size=$3
+#pop2_sample_size=$4
+#pop3_sample_size=$5
+
+
+#python model_admix.py 1000 5000 1000 6000 0.8 0.2 $chrom \
+#$dem_option $pop1_sample_size $pop2_sample_size $pop3_sample_size
+
+chrom=$1
+dem_option=$2
+sample_pop1=$3
+
+python msprime_demographies.py $chrom $dem_option $sample_pop1
+
+module load PLINK/1.9b_6.13-x86_64
+
+plink --vcf $chrom.$dem_option.$sample_pop1.vcf --double-id --make-bed --out $chrom.$dem_option.$sample_pop1
+
+python Dependencies/bim_fix.py $chrom.$dem_option.$sample_pop1 $chrom.$dem_option.$sample_pop1.fixed.bim $chrom
+
+mv $chrom.$dem_option.$sample_pop1.fixed.bim $chrom.$dem_option.$sample_pop1.bim
+
+plink --bfile $chrom.$dem_option.$sample_pop1 --recode vcf-iid --out $chrom.$dem_option.$sample_pop1
